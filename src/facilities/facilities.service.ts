9946c715669082b1bdd75126fe439767dd96d780
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-05-26 09:41:19 -0400

    Fixed unresolved merge issue

--- src/facilities/facilities.service.ts
+++ src/facilities/facilities.service.ts
@@ -116,14 +116,7 @@ export class FacilitiesService {
       },
     });
 
-<<<<<<< HEAD
-    const [sql, values] = await this.repository.buildQuery(
-      fieldMappingsList,
-      params,
-    );
-=======
     const [sql, values] = await this.repository.buildQuery(params);
->>>>>>> 632dc81f6e135a7719dcbd880f5a12bf85eaefaf
 
     return this.streamingService.getStream(
       req,
