a86ccf18608070242e3dbc54f04c09aa17c6afa3
abufofanah <cvp@Abus-MBP.lan>
2023-07-25 09:44:03 -0400

    updated summary value endpoint

--- src/summary-value/summary-value.service.ts
+++ src/summary-value/summary-value.service.ts
@@ -26,7 +26,6 @@ export class SummaryValueService {
     const json2Dto = new Transform({
       objectMode: true,
       transform(data, _enc, callback) {
-        delete data.id;
         const dto = plainToClass(SummaryValueBaseDTO, data, {
           enableImplicitConversion: false,
         });
