e4c8a822682640095b53c46545464637e0c06cec
abufofanah <cvp@Abus-MBP.lan>
2023-07-26 14:46:10 -0400

    updated format for addDate and updateDate

--- src/dto/summary-value.dto.ts
+++ src/dto/summary-value.dto.ts
@@ -1,4 +1,6 @@
+import { Transform } from 'class-transformer';
 import { IsNumber, IsString } from 'class-validator';
+import moment from 'moment';
 
 export class SummaryValueBaseDTO {
   @IsString()
@@ -26,8 +28,10 @@ export class SummaryValueBaseDTO {
   userId: string;
 
   @IsString()
+  @Transform(date => moment(new Date(date.value)).format('YYYY/MM/DD HH:mm:ss'))
   addDate: string;
 
   @IsString()
+  @Transform(date => date.value? moment(new Date(date.value)).format('YYYY/MM/DD HH:mm:ss') : null)
   updateDate: string;
 }
--- src/dto/supplemental-operating.dto.ts
+++ src/dto/supplemental-operating.dto.ts
@@ -1,19 +1,33 @@
+import { Transform } from "class-transformer";
+import { IsNumber, IsString } from "class-validator";
+import moment from "moment";
+
 export class SupplementalOperatingDTO {
+  @IsString()
   id: string;
 
-  locationId?: string;
+  @IsString()
+  locationId: string;
 
+  @IsNumber()
   reportPeriodId: number;
 
+  @IsString()
   operatingTypeCode: string;
 
+  @IsString()
   fuelCode: string;
 
+  @IsNumber()
   operatingValue: number;
-
+  @IsString()
   userId: string;
 
+  @IsString()
+  @Transform(date => moment(new Date(date.value)).format('YYYY/MM/DD HH:mm:ss'))
   addDate: string;
 
+  @IsString()
+  @Transform(date => date.value? moment(new Date(date.value)).format('YYYY/MM/DD HH:mm:ss') : null)
   updateDate: string;
 }
