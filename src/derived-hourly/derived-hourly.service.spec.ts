30ef9735aa889a5624b993c03a17de7346ee6064
spetros-do <153679433+spetros-do@users.noreply.github.com>
2024-07-24 14:37:59 -0700

    Updated file to include needed files.

--- src/derived-hourly/derived-hourly.service.spec.ts
+++ src/derived-hourly/derived-hourly.service.spec.ts
@@ -1,9 +1,56 @@
 import { Test } from '@nestjs/testing';
-import { EntityManager, SelectQueryBuilder } from 'typeorm';
+//import { EntityManager, SelectQueryBuilder } from 'typeorm';
+import { DerivedHourlyService } from './derived-hourly.service';
+import { StreamingService } from '../streaming/streaming.service';
 import { DerivedHourlyRepository } from './derived-hourly.repository';
-import { DerivedHrlyValue } from '../entities/derived-hrly-value.entity';
+//import { DerivedHrlyValue } from '../entities/derived-hrly-value.entity';
 import { HourlyParamsDto } from '../dto/derived-hourly-value.params.dto';
 
+describe('-- Derived Hourly Value Service --', () => {
+  let service: DerivedHourlyService;
+  let streamingService: StreamingService;
+  let repository: DerivedHourlyRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        DerivedHourlyService,
+        {
+          provide: StreamingService,
+          useFactory: () => ({
+            getStream: jest.fn(),
+          }),
+        },
+        {
+          provide: DerivedHourlyRepository,
+          useFactory: () => ({
+            buildQuery: jest.fn().mockResolvedValue(['', '']),
+          }),
+        },
+      ],
+    }).compile();
+
+    service = module.get(DerivedHourlyService);
+    streamingService = module.get(StreamingService);
+    repository = module.get(DerivedHourlyRepository);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('streamValues', () => {
+    it('Should call the repo build query and return a StreamableFile from StreamingService', async () => {
+      const params = new HourlyParamsDto();
+      await service.streamValues(null, params);
+      expect(repository.buildQuery).toHaveBeenCalledWith(params);
+      expect(streamingService.getStream).toHaveBeenCalled();
+    });
+  });
+});
+
+
+/* The previous code in this file before adding above code.
 describe('-- Derived Hourly Value Repository --', () => {
   let repo: DerivedHourlyRepository;
   let mockedQueryBuilder: jest.Mocked<SelectQueryBuilder<DerivedHrlyValue>>;
@@ -44,3 +91,4 @@ describe('-- Derived Hourly Value Repository --', () => {
     });
   });
 });
+*/
