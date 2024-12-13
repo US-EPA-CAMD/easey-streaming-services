44a576525804a64bd3d03502c472823d060b4a31
ergjustin <justin.joseph.erg.com>
2024-12-13 12:07:49 -0600

    include apiKey valude in config file

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -75,4 +75,5 @@ export default registerAs('app', () => ({
   authApi: {
     uri: getConfigValue('EASEY_AUTH_API', `https://${apiHost}/auth-mgmt`),
   },
+  apiKey: getConfigValue('EASEY_STREAMING_SERVICES_API_KEY'),
 }));
